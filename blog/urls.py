b3537627c60109c6ae0ce51c550aaceac707f4eb
anaikasmile <eliora20147@gmail.com>
2017-06-05 17:59:55 +0200

    Template modification

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
-	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail')
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+	url(r'^post/new/$', views.post_view,name='post_new'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+	url(r'^post/draft/list/$', views.post_draft_list, name='post_draft_list'),
+	url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
+	url(r'^post/(?P<pk>[0-9]+)/delete/$', views.post_delete, name='post_delete'),
 ]
